2ada02e604a53147e4e73c405771c7f4aca23d92
Matt Olm <mattolm@gmail.com>
2019-11-13 15:29:46 -0800

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Matt Olm and Alex Crits-Christoph'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.2'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
