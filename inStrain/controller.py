227d9033aaaf3a82bdec2d60b2bf7312fe62fe87
Matt Olm <mattolm@gmail.com>
2020-09-14 09:43:03 -0700

    1.3.6

--- inStrain/controller.py
+++ inStrain/controller.py
@@ -11,6 +11,11 @@ import pysam
 import logging
 import argparse
 import pandas as pd
+import functools
+import logging
+import struct
+import sys
+
 
 from subprocess import call
 from datetime import datetime
@@ -36,6 +41,9 @@ class Controller():
 
     def main(self, args):
         ''' Parse user options and call the correct pipeline'''
+        # Patch multiprocessing
+        patch_mp_connection_bpo_17560()
+
         # Call the appropriate workflow
         if args.operation == "profile":
             self.profile_operation(args)
@@ -415,3 +423,64 @@ def setup_logger(loc):
     logging.debug("Command to run inStrain was: {0}\n".format(' '.join(sys.argv)))
     logging.debug("inStrain version {0} was run \n".format(__version__))
     logging.debug("!"*80 + '\n')
+
+
+def patch_mp_connection_bpo_17560():
+    """
+    From https://stackoverflow.com/questions/47776486/python-struct-error-i-format-requires-2147483648-number-2147483647
+
+    Apply PR-10305 / bpo-17560 connection send/receive max size update
+
+    See the original issue at https://bugs.python.org/issue17560 and
+    https://github.com/python/cpython/pull/10305 for the pull request.
+
+    This only supports Python versions 3.3 - 3.7, this function
+    does nothing for Python versions outside of that range.
+
+    """
+    patchname = "Multiprocessing connection patch for bpo-17560"
+    if not (3, 3) < sys.version_info < (3, 8):
+        print(
+            patchname + " not applied, not an applicable Python version: %s",
+            sys.version
+        )
+        return
+
+    from multiprocessing.connection import Connection
+
+    orig_send_bytes = Connection._send_bytes
+    orig_recv_bytes = Connection._recv_bytes
+    if (
+        orig_send_bytes.__code__.co_filename == __file__
+        and orig_recv_bytes.__code__.co_filename == __file__
+    ):
+        print(patchname + " already applied, skipping")
+        return
+
+    @functools.wraps(orig_send_bytes)
+    def send_bytes(self, buf):
+        n = len(buf)
+        if n > 0x7fffffff:
+            pre_header = struct.pack("!i", -1)
+            header = struct.pack("!Q", n)
+            self._send(pre_header)
+            self._send(header)
+            self._send(buf)
+        else:
+            orig_send_bytes(self, buf)
+
+    @functools.wraps(orig_recv_bytes)
+    def recv_bytes(self, maxsize=None):
+        buf = self._recv(4)
+        size, = struct.unpack("!i", buf.getvalue())
+        if size == -1:
+            buf = self._recv(8)
+            size, = struct.unpack("!Q", buf.getvalue())
+        if maxsize is not None and size > maxsize:
+            return None
+        return self._recv(size)
+
+    Connection._send_bytes = send_bytes
+    Connection._recv_bytes = recv_bytes
+
+    print(patchname + " applied")
